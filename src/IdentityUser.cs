26116c4fe2e5fb72c6a770f3c5a0276e5c9a4b8e
allan-mobley-jr <allan.mobley.jr@mobsites.com>
2019-10-28 19:37:02 -0400

    Added support for partitions.

--- src/IdentityUser.cs
+++ src/IdentityUser.cs
@@ -23,5 +23,13 @@ namespace AspNetCore.Identity.Cosmos
                 id = value ?? base.Id;
             }
         }
+
+        /// <summary>
+        ///     Override this to provide a value for the partition key parameter in the Cosmos container method calls.
+        ///     NOTE: The derived class must also include a property that matches the partition key path that was used when creating the container.
+        /// </summary>
+        [Newtonsoft.Json.JsonIgnore]
+        [System.Text.Json.Serialization.JsonIgnore]
+        public virtual string PartitionKey => null;
     }
 }
--- src/IdentityUserLogin.cs
+++ src/IdentityUserLogin.cs
@@ -25,5 +25,13 @@ namespace AspNetCore.Identity.Cosmos
                 id = value ?? Guid.NewGuid().ToString();
             }
         }
+
+        /// <summary>
+        ///     Override this to provide a value for the partition key parameter in the Cosmos container method calls.
+        ///     NOTE: The derived class must also include a property that matches the partition key path that was used when creating the container.
+        /// </summary>
+        [Newtonsoft.Json.JsonIgnore]
+        [System.Text.Json.Serialization.JsonIgnore]
+        public virtual string PartitionKey => null;
     }
 }
