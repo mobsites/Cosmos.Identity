5abb0b8c281ccf9f4497576bd3a239989b4ccfbb
allan-mobley-jr <allan.mobley.jr@mobsites.com>
2019-10-28 14:24:21 -0400

    Added override Id poperty.

--- src/IdentityUser.cs
+++ src/IdentityUser.cs
@@ -6,5 +6,20 @@ namespace AspNetCore.Identity.Cosmos
 {
     class IdentityUser : Microsoft.AspNetCore.Identity.IdentityUser
     {
+        private string id;
+
+        /// <summary>
+        ///     Hide base class property so that it can be serialized correctly as primary key "id" for Cosmos.
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty(PropertyName = "id")]
+        [System.Text.Json.Serialization.JsonPropertyName("id")]
+        public new string Id
+        {
+            get => id;
+            set
+            {
+                id = value ?? base.Id;
+            }
+        }
     }
 }
