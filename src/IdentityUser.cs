4e4cfee71a28d3ee159dcd15db9e5dcf9e791cc8
allan-mobley-jr <allan.mobley.jr@mobsites.com>
2019-10-28 12:35:16 -0400

    Add project files.

---
+++ src/IdentityUser.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspNetCore.Identity.Cosmos
+{
+    class IdentityUser : Microsoft.AspNetCore.Identity.IdentityUser
+    {
+    }
+}
