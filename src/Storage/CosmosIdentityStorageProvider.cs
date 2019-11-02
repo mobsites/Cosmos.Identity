1c0e544e9e1238583d0d0b084a5c2e3e08231114
allan.mobley,jr <allan.mobley.jr@mobsites.com>
2019-11-02 09:34:20 -0400

    Moved container interface to new storage folder and added empty storage provider class and interface.

---
+++ src/Storage/CosmosIdentityStorageProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mobsites.AspNetCore.Identity.Cosmos
+{
+    public class CosmosIdentityStorageProvider : ICosmosIdentityStorageProvider
+    {
+    }
+}
